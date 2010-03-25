379f47a48bfb973f661e8030d556afa34a35c200
tav <tav@espians.com>
2010-03-25 13:39:45 +0000

    Added pycrypto 2.1.0.

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@
 import sys
 
 from distutils.core import Extension, setup
+from os.path import dirname, join as join_path, realpath
+
+from pyutil.env import run_command
 
 # ------------------------------------------------------------------------------
 # the extensions
@@ -24,4 +27,12 @@ setup(
     version="git",
     description="A collection of Python libraries",
     ext_modules=extensions,
+    )
+
+pycrypto_path = join_path(dirname(realpath(__file__)), 'pycrypto')
+
+run_command(
+    [sys.executable, join_path(pycrypto_path, 'setup.py'), 'build_ext', '-i'],
+    exit_on_error=True, cwd=join_path(pycrypto_path), redirect_stdout=False,
+    redirect_stderr=False
     )
