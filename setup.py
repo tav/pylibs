3273b85c8d84c8b2920c543dfa175fd1b8366b42
tav <tav@espians.com>
2011-02-24 05:17:27 +0000

    Added the pycrypto package to setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 #! /usr/bin/env python
 
+# Public Domain (-) 2010-2011 The Ampify Authors.
+# See the Ampify UNLICENSE file for details.
+
 import sys
 
 from distutils.core import Extension, setup
 from os.path import dirname, join as join_path, realpath
 
-from pyutil.env import run_command
+try:
+    from pyutil.env import run_command
+except ImportError:
+    from subprocess import call
+    def run_command(args, cwd, **kwargs):
+        retcode = call(args, cwd=cwd)
+        if retcode and kwargs.pop('exit_on_error', None):
+            sys.exit(1)
 
 # ------------------------------------------------------------------------------
 # the extensions
@@ -31,12 +41,15 @@ setup(
     )
 
 pylibs_path = dirname(realpath(__file__))
-ssl_path = join_path(pylibs_path, 'pyssl')
+packages_path = ['pycrypto']
 
 if sys.version_info < (2, 6):
-    for path in [ssl_path]:
-        run_command(
-            [sys.executable, join_path(path, 'setup.py'), 'build_ext', '-i'],
-            exit_on_error=True, cwd=join_path(path), redirect_stdout=False,
-            redirect_stderr=False
-            )
+    packages_path.append('pyssl')
+
+for path in packages_path:
+    path = join_path(pylibs_path, path)
+    run_command(
+        [sys.executable, join_path(path, 'setup.py')] + sys.argv[1:],
+        exit_on_error=True, cwd=path, redirect_stdout=False,
+        redirect_stderr=False
+        )
